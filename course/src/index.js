30fe32a609179a1dfc035abe7ce14e494a0624bd
jasquier <jasquier@gmail.com>
2018-12-22 08:02:14 -0500

    Finished the linting section.

--- course/src/index.js
+++ course/src/index.js
@@ -4,5 +4,5 @@ import numeral from 'numeral';
 
 // TODO poke around with numeral
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
+// eslint-disable-next-line
 console.log(`I would pay ${courseValue} for this awesome course!`);
